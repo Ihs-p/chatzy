d71cca8e7f5672752b8f071ec363714560b0458b
ihsan <ihsanp3553@gmail.com>
2025-02-12 15:49:01 +0530

    Fix frontend serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,57 +1,42 @@
-import express from 'express';
-import cors from 'cors';
-import { connectDB } from './lib/db.js';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import { app, server } from './lib/socket.js';
-import { fileURLToPath } from 'url';
-import path from 'path';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 5000;
-const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173';
-
-// Middleware
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use(cors({
-    origin: CLIENT_URL,
-    credentials: true
-}));
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-// Routes
-const authRoute = (await import('./routes/auth.route.js')).default;
-const messageRoute = (await import('./routes/message.route.js')).default;
-app.use('/api/auth', authRoute);
-app.use('/api/messages', messageRoute);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get('/', (req, res) => {
-    res.send('Chat API is working!');
-});
-
-// Serve Frontend in Production
-if (process.env.NODE_ENV === 'production') {
-    // app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.use(express.static(path.join(__dirname, 'frontend/dist')));
-
-
-    app.get('*', (req, res, next) => {
-        const indexPath = path.join(__dirname, '../frontend/dist/index.html');
-        res.sendFile(indexPath, (err) => {
-            if (err) {
-                next(err);
-            }
-        });
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-// Start Server
 server.listen(PORT, () => {
-    console.log(`App listening on port: ${PORT} in ${process.env.NODE_ENV} mode`);
-    connectDB();
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
